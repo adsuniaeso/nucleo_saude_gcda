d9cca8967b1b5608aa715c02687c82a8ad2c255b
guilherme-ads-uniaeso <guilherme.cavalvanti@aluno.uniaeso.edu.br>
2021-11-22 12:42:56 -0300

    Adicionando projeto no GitHub

---
+++ cmake-build-debug/CMakeFiles/ProjetoSaude.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetoSaude.dir/main.c.obj"
+  "ProjetoSaude.exe"
+  "ProjetoSaude.exe.manifest"
+  "ProjetoSaude.pdb"
+  "libProjetoSaude.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProjetoSaude.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
